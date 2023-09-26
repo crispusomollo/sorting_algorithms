724d16de3acdbc462eafc79e8efe9fea1a6ba00c
crispusomollo <crispusomollo@gmail.com>
2023-09-26 00:27:55 -0700

    sort header

--- sort.h
+++ sort.h
@@ -9,13 +9,13 @@
 /**
  * struct listint_s - Doubly linked list node
  *
- * @j: Integer stored in the node
+ * @n: Integer stored in the node
  * @next: Pointer to the next element of the list
  * @prev: Pointer to the previous element of the list
  */
 typedef struct listint_s
 {
-	const int j;
+	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
